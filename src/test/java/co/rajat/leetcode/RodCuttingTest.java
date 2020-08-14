007d8b1f8a2657a8b3a3806c0fd9c6a181272a4f
Rajat Arora <rajat@rajatarora.info>
2020-08-14 12:37:25 +0530

    Fix typo in Rod Cutting problem

--- src/test/java/co/rajat/leetcode/RodCuttingTest.java
+++ src/test/java/co/rajat/leetcode/RodCuttingTest.java
@@ -28,7 +28,7 @@ public class RodCuttingTest {
     @Test
     public void cutRodIterative_13() {
 
-        int[] prices = { 1, 2, 4, 4, 6, 6, 6, 8, 9, 11, 11, 16, 17 };
+        int[] prices = { 1, 2, 4, 4, 6, 6, 6, 8, 9, 11, 11, 16, 17, 17 };
         int result = objectUnderTest.cutRodIterative(prices, 13);
         assertThat(result, is(117));
     }
